7a7eed4b276b2b70d2dfccc2421c2b6b45c447a5
Andrey Kodyk <andreykodyk@Air-von-Andrey.netis.cc>
2025-04-19 09:17:15 +0400

    Revert "Merge pull request #4 from AnatoliyAksenov/feature/recommendation"
    This reverts commit 5fa640a12de632252368e0dd71b4f212bb95e83a, reversing
    changes made to 62343e6ab1178cb486c5bfdbb70a1ccee49c2322.

--- app/agents/evaluation_agent.py
+++ app/agents/evaluation_agent.py
@@ -3,7 +3,6 @@ from app.agents.tools.extract_situation import extract_situation
 from app.agents.tools.extract_task import extract_task
 from app.agents.tools.extract_action import extract_action
 from app.agents.tools.extract_result import extract_result
-from app.agents.tools.extract_recommendation import extract_recommendation
 from pydantic import BaseModel
 
 
@@ -12,13 +11,12 @@ class STAROutput(BaseModel):
     Task: str
     Action: str
     Result: str
-    Recommendation: str
 
 def create_evaluation_agent(system_prompt: str) -> Agent:
     return Agent(
         name="Evaluation Agent",
         handoff_description="Оценка сообщений с использованием метода STAR.",
         instructions=system_prompt,
-        tools=[extract_situation, extract_action, extract_task, extract_result, extract_recommendation],
+        tools=[extract_situation, extract_action, extract_task, extract_result],
         output_type=STAROutput
     )
